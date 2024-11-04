071129d0d67f43b8b76003f09e9385d4dd31c0eb
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2024-11-04 05:59:54 +0500

    book title animation changed

--- app/(root)/(home)/_components/books.tsx
+++ app/(root)/(home)/_components/books.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useRef, useState } from 'react'
 import { GiBookshelf } from 'react-icons/gi'
+
 function Books() {
 	const [isVisible, setIsVisible] = useState(false)
 	const newsRef = useRef<HTMLDivElement | null>(null)
@@ -24,7 +25,7 @@ function Books() {
 				})
 			},
 			{
-				threshold: 0.9,
+				threshold: 0.1,
 			}
 		)
 
@@ -38,6 +39,7 @@ function Books() {
 			}
 		}
 	}, [])
+
 	return (
 		<>
 			<div className='my-4 overflow-hidden'>
@@ -45,23 +47,23 @@ function Books() {
 					<GiBookshelf
 						className={cn(
 							'h-20 text-5xl text-blue-500 transition-transform duration-500 ease-in-out',
-							isVisible ? 'translate-y-0' : '-translate-y-20'
+							isVisible ? '-translate-y-5' : '-translate-y-20'
 						)}
 					/>
 					<h1
 						className={cn(
 							'h-20 bg-gradient-to-r from-blue-700 to-blue-400 bg-clip-text text-center font-serif text-5xl font-bold text-transparent transition-transform duration-1000 ease-in-out',
-							isVisible ? 'translate-y-4' : '-translate-y-20'
+							isVisible ? 'translate-y-0' : '-translate-y-20'
 						)}
 					>
 						Kitoblar
 					</h1>
 				</div>
 
-				<div className='grid grid-cols-4 gap-4' ref={newsRef}>
+				<div className='grid grid-cols-4 gap-4'>
 					<div className='col-span-1'>
-						<div className=''>
-							<p>Elektron kitoblar biror ta&apos;rif </p>
+						<div>
+							<p>Elektron kitoblar biror ta&apos;rif</p>
 							<Image
 								src={'/images/camels.svg'}
 								alt='camels'
@@ -70,7 +72,7 @@ function Books() {
 							/>
 						</div>
 					</div>
-					<div className='col-span-3 grid grid-cols-4 gap-3'>
+					<div className='col-span-3 grid grid-cols-4 gap-3' ref={newsRef}>
 						{books.map(book => (
 							<Card
 								key={book.title}
@@ -83,12 +85,16 @@ function Books() {
 						))}
 					</div>
 				</div>
-				<Button
-					className='mx-auto my-10 flex shadow-[0px_0px_12px_#0959a9]'
-					variant={'default'}
-				>
-					<Link href={'/books'}>Ko&apos;proq ko&apos;rish</Link>
-				</Button>
+
+				{/* Link wrapped around Button to avoid potential rendering issues */}
+				<Link href='/books' passHref>
+					<Button
+						className='mx-auto my-10 flex shadow-[0px_0px_12px_#0959a9]'
+						variant={'default'}
+					>
+						Ko&apos;proq ko&apos;rish
+					</Button>
+				</Link>
 			</div>
 		</>
 	)
