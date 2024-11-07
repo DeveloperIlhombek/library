c3e76dd808266942bd0be15c203c14808a2c4a1d
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2024-11-07 17:18:25 +0500

    No center page

--- app/(root)/center/page.tsx
+++
@@ -1,9 +0,0 @@
-import Test from '@/components/parallax/center-parallex'
-
-export default function Page() {
-	return (
-		<main className='flex min-h-[300vh] flex-col items-center justify-between p-24'>
-			<Test />
-		</main>
-	)
-}
