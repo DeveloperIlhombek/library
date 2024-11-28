56577eb744002747b04fa4c910914e168b4aac84
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2024-11-28 15:42:51 +0500

    language layout

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -8,8 +8,5 @@ export const routing = defineRouting({
 	// Used when no locale matches
 	defaultLocale: 'uz',
 })
-
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter } =
 	createNavigation(routing)
